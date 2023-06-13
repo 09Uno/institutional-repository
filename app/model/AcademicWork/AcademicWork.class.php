d3ea575202e7ab3a244a9f4b12307dedeff921d9
09Uno <bruno.a.oliveira.1209@gmail.com>
2023-06-13 19:32:38 -0300

    first commit

---
+++ app/model/AcademicWork/AcademicWork.class.php
@@ -0,0 +1,32 @@
+<?php
+use Adianti\Database\TRecord;
+
+
+
+class AcademicWork extends TRecord{
+
+    const TABLENAME = 'academics_works';
+    const PRIMARYKEY = 'id';
+    const IDPOLICY = 'serial';
+
+
+    public function __construct($id = null){
+
+        parent::__construct($id);
+        parent::addAttribute('title');
+        parent::addAttribute('author');
+        parent::addAttribute('advisor');
+        // parent::addAttribute('co_advisor');
+        parent::addAttribute('abstract');
+        parent::addAttribute('keywords');
+        parent::addAttribute('presentation_date');
+        parent::addAttribute('research_area');
+        parent::addAttribute('file');
+
+    }
+
+
+
+
+
+}
